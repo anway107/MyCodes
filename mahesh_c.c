65b54d173a4d5326e70277749a3e051dad4f902a
akshay3219 <akshaymadake3219@yahoo.com>
2016-08-12 17:19:45 +0530

    interchanging of two variables using third variable

---
+++ mahesh_c.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+int main()
+{
+
+  int a,b,c;
+printf("Enter the values of a,b");
+scanf("%d%d",&a,&b);
+
+//interchanging formula
+
+c=a;
+a=b;
+b=c;
+
+printf("interchanging values of a and b is %d\n %d\n",a,b);
+
+return 0;
+
+
+}
+
