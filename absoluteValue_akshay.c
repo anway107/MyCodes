afbf55fe34319c712c0c74f063b928eace2eb670
akshay <akshaymadke1@gmail.com>
2016-08-13 12:48:21 +0530

    askCode12

---
+++ absoluteValue_akshay.c
@@ -0,0 +1,27 @@
+
+/**Date:13/8/16
+Absolute value:In mathematics, the absolute value or modulus |x| of a real number x is
+ the non-negative value of x without regard to its sign.
+  **/
+
+
+
+#include<stdio.h>
+
+void main()
+{int a,b;
+printf("enter a number");
+scanf("%d",&a);
+
+if(a<0)
+ b=-a;
+
+else
+
+ b=a;
+
+printf("Absolute value of entered number is %d",b);
+
+
+
+}
---
+++ greatestNumber_akshay.c
@@ -0,0 +1,18 @@
+﻿/**Greatest among three numbres using ternary operator**/
+
+#include<stdio.h>
+
+int main()
+{
+int a,b,c,greatest;
+
+printf("enter three numbers");
+scanf("%d %d %d",&a,&b,&c);
+
+greatest=(a>b)?(a>c?a:c):(b>c?b:c);
+
+printf("greatest number is %d",greatest);
+
+
+
+}
