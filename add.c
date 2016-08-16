6d76535bc4356308c021cf1a02dcfb4c717f9a52
akshay3219 <akshaymadake3219@yahoo.com>
2016-08-16 15:24:39 +0530

    addition of two number by mahesh

---
+++ add.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+int main()
+{
+  int m,n,o;
+
+ printf("Enter the fisrt Number\n");
+ scanf("%d",&m);
+
+printf("Enter the second Number\n");
+ scanf("%d",&n);
+
+
+//Formula 
+o = m + n;
+
+printf("Addition of two number is %d\n",o);
+
+return 0;
+
+}
